ea47c0083e71ebe4d655fc9031fb292a66e3fbf8
EffyJohn <effyoommen@hotmail.com>
2020-06-02 01:58:01 +0530

    Simple navbar that works on PC but is broken on mobile

--- bitsmun.js
+++ bitsmun.js
@@ -1,7 +1,20 @@
 let navbar = document.getElementsByClassName('navbar')[0];
 let navToggle = document.getElementsByClassName('navbar__toggle')[0];
 let loadTrigger = document.getElementsByClassName('load-trigger')[0];
+let navMenu = document.getElementsByClassName('navbar__menu')[0];
 
-navbar.classList.append("hidden");
-navToggle.classList.append("stowed");
+navbar.classList.add("hidden");
+navToggle.classList.add("stowed");
+navMenu.classList.add("stowed");
 
+navToggle.addEventListener('click', function(){
+    if (navbar.classList.contains("hidden")){
+        navbar.classList.remove("hidden");
+        navToggle.classList.remove("stowed");
+        navMenu.classList.remove("stowed");
+    } else {
+        navbar.classList.add("hidden");
+        navToggle.classList.add("stowed");
+        navMenu.classList.add("stowed");
+    }
+});
