6c8db7dcb3644c9d480f613f9663aef21c01ae48
antares511 <37109848+antares511@users.noreply.github.com>
2020-06-02 19:19:27 +0530

    Update bitsmun.js

--- bitsmun.js
+++ bitsmun.js
@@ -1,24 +1,24 @@
-let navbar = document.getElementsByClassName("navbar")[0];
-let navToggle = document.getElementsByClassName("navbar__toggle")[0];
-let loadTrigger = document.getElementsByClassName("load-trigger")[0];
-let navMenu = document.getElementsByClassName("navbar__menu")[0];
-let navbarTop = document.getElementsByClassName("navbar__top")[0];
+let navbar = document.getElementsByClassName('navbar')[0];
+let navToggle = document.getElementsByClassName('navbar__toggle')[0];
+let loadTrigger = document.getElementsByClassName('load-trigger')[0];
+let navMenu = document.getElementsByClassName('navbar__menu')[0];
+let navTop = document.getElementsByClassName('navbar__top')[0];
 
 navbar.classList.add("hidden");
 navToggle.classList.add("stowed");
 navMenu.classList.add("stowed");
-navbarTop.classList.add("stowed");
+navTop.classList.add("stowed");
 
-navToggle.addEventListener("click", function () {
-  if (navbar.classList.contains("hidden")) {
-    navbar.classList.remove("hidden");
-    navbarTop.classList.remove("stowed");
-    navToggle.classList.remove("stowed");
-    navMenu.classList.remove("stowed");
-  } else {
-    navbarTop.classList.add("stowed");
-    navbar.classList.add("hidden");
-    navToggle.classList.add("stowed");
-    navMenu.classList.add("stowed");
-  }
+navToggle.addEventListener('click', function(){
+    if (navbar.classList.contains("hidden")){
+        navbar.classList.remove("hidden");
+        navToggle.classList.remove("stowed");
+        navMenu.classList.remove("stowed");
+        navTop.classList.remove("stowed");
+    } else {
+        navbar.classList.add("hidden");
+        navToggle.classList.add("stowed");
+        navMenu.classList.add("stowed");
+        navTop.classList.add("stowed");
+    }
 });
